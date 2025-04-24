414b6500b3546b6abfa26a60b59e0d54a828108e
Andrea Pierré <andrea_pierre@brown.edu>
2025-04-23 23:05:32 -0400

    Rename envs + add Door+cube env

---
+++ Multi-Doors/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import robosuite
+
+from door_custom import DoorCustom
+
+# create environment instance
+env = robosuite.make(
+    env_name="DoorCustom",  # try with other tasks like "Stack" and "Door"
+    robots="UR5e",  # try with other robots like "Sawyer" and "Jaco"
+    has_renderer=True,
+    has_offscreen_renderer=False,
+    use_camera_obs=False,
+)
+
+
+# reset the environment
+env.reset()
+
+for i in range(100):
+    action = np.random.randn(*env.action_spec[0].shape) * 0.1
+    obs, reward, done, info = env.step(action)  # take action in the environment
+    env.render()  # render on display
